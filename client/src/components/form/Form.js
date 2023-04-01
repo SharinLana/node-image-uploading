da8b2fbcd8df96908906525b004345f8f7728940
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-03-31 18:38:05 -0700

    used FormInput components inside of the Form + passed the props to the inputs

--- client/src/components/form/Form.js
+++ client/src/components/form/Form.js
@@ -1,8 +1,12 @@
 import React from "react";
+import FormInput from "./FormInput";
 
 const Form = () => {
   return (
     <form>
+      <FormInput labelText="Product title" name="title" type="text" />
+      <FormInput labelText="Product price" name="price" type="text" />
+      <FormInput labelText="Product image" name="image" type="file" />
       <button type="submit">Submit</button>
     </form>
   );

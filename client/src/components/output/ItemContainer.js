42848e42a9fabe4abadba6db22de1cab5a2bd2dc
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-03-31 18:47:44 -0700

    created an array of dummy items and passed each item to the child component Item.js

--- client/src/components/output/ItemContainer.js
+++ client/src/components/output/ItemContainer.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import Item from "./Item";
 
+const dummyItems = [
+  {
+    id: 1,
+    title: "Phone",
+    price: 450,
+    image: "",
+  },
+  {
+    id: 2,
+    title: "Keyboard",
+    price: 150,
+    image: "",
+  },
+  {
+    id: 3,
+    title: "Mouse",
+    price: 250,
+    image: "",
+  },
+];
 const ItemContainer = () => {
   return (
-    <div>ItemContainer</div>
-  )
-}
+    <div>
+      {dummyItems.map((item) => {
+        return (
+          <Item
+            key={item.id}
+            title={item.title}
+            price={item.price}
+            image={item.image}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
-export default ItemContainer
+export default ItemContainer;

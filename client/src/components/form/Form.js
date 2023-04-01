bc85af4413f344e407a9e76ec7c3e9ec1b560faf
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-03-31 18:40:36 -0700

    added a header to the Form component + changed the label text

--- client/src/components/form/Form.js
+++ client/src/components/form/Form.js
@@ -4,9 +4,10 @@ import FormInput from "./FormInput";
 const Form = () => {
   return (
     <form>
-      <FormInput labelText="Product title" name="title" type="text" />
-      <FormInput labelText="Product price" name="price" type="text" />
-      <FormInput labelText="Product image" name="image" type="file" />
+      <h3>Product Card</h3>
+      <FormInput labelText="Title" name="title" type="text" />
+      <FormInput labelText="Price" name="price" type="text" />
+      <FormInput labelText="Image" name="image" type="file" />
       <button type="submit">Submit</button>
     </form>
   );

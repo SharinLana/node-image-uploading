97c7a47427ffc17d3c43efe4dacb18ce193c30d9
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-02 16:14:01 -0700

    created product model

---
+++ models/productModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;

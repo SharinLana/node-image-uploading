26f540b0363a095f9e5d01b1cd3c285135f6f8ed
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-02 15:51:50 -0700

    connected the app to the MongoDB database

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
+const dotenv = require("dotenv");
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
 
 const port = process.env.port || 3100;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+const start = async () => {
+  try {
+    await mongoose
+      .connect(process.env.MONGODB_URL)
+      .then(() => console.log("MONGODB connected!"))
+      .catch((err) => console.log(err));
+
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();

841c8ec0b241ab3b11327cba087ff7bb208d8477
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-02 19:32:55 -0700

    invoked the context function getAllProducts in the ItenContainer component and displayed all the products on the screen

--- client/src/components/output/ItemContainer.js
+++ client/src/components/output/ItemContainer.js
@@ -1,33 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppContext } from "../../context/appContext";
 import Item from "./Item";
 
-const dummyItems = [
-  {
-    id: 1,
-    title: "Phone",
-    price: 450,
-    image: "",
-  },
-  {
-    id: 2,
-    title: "Keyboard",
-    price: 150,
-    image: "",
-  },
-  {
-    id: 3,
-    title: "Mouse",
-    price: 250,
-    image: "",
-  },
-];
 const ItemContainer = () => {
+  const { getAllProducts, products } = useAppContext();
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
     <div className="output-container">
-      {dummyItems.map((item) => {
+      {products.map((item) => {
         return (
           <Item
-            key={item.id}
+            key={item._id}
             title={item.title}
             price={item.price}
             image={item.image}

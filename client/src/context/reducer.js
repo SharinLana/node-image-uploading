c2dc123e4ab2d373c7cfc4e859d3704583fcb19e
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-02 19:31:43 -0700

    created a reducer for the GET_PRODUCTS action type

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,6 +1,9 @@
 import { GET_PRODUCTS } from "./actions";
 
 const reducer = (state, action) => {
+  if (action.type === GET_PRODUCTS) {
+    return { ...state, products: action.payload.products };
+  }
   throw new Error(`No such action: ${action.type}`);
 };
 

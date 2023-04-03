0c8de9db459be13cfc7243b35c3e461e06e765c1
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-02 17:27:01 -0700

    finalized the uploadImage controller by creating a path to store the images, moving the uploaded images to that path and sending the response to the client

--- controllers/uploads-controllers.js
+++ controllers/uploads-controllers.js
@@ -1,8 +1,23 @@
+const path = require("path");
 const Product = require("../models/productModel");
 const { StatusCodes } = require("http-status-codes");
 
 const uploadImage = async (req, res, next) => {
-  res.send("Upload Image");
+  // console.log(req.files);
+  const productImage = req.files.image;
+
+  const imagePath = path.join(
+    __dirname,
+    "../client/public/uploads/" + `${productImage.name}`
+  );
+  // then create an "uploads" folder in the "client/public"
+
+  await productImage.mv(imagePath); //mv() is an express-fileupload method
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/public/uploads/${productImage.name}` } });
+  // Then, before checking the result via Postman, use express static method in server.js
+  // to make this file publicly available
 };
 
 module.exports = { uploadImage };

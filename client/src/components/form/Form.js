8b6991915c389fccb7d96866492183c783b6c5a5
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-02 21:36:42 -0700

    invoked the getValue context function inside of the form components

--- client/src/components/form/Form.js
+++ client/src/components/form/Form.js
@@ -1,14 +1,44 @@
 import React from "react";
+import { useAppContext } from "../../context/appContext";
 import FormInput from "./FormInput";
 
 const Form = () => {
+  const { getValue, title, price, image } = useAppContext();
+
+  const getInputValueHandler = (e) => {
+    getValue({ name: e.target.name, value: e.target.value });
+  };
+
   return (
     <form>
       <h3 className="form-header">Product Card</h3>
-      <FormInput labelText="Title" name="title" type="text" className="input" />
-      <FormInput labelText="Price" name="price" type="text" className="input" />
-      <FormInput labelText="Image" name="image" type="file" className="file" />
-      <button type="submit" className="btn">Submit</button>
+      <FormInput
+        labelText="Title"
+        name="title"
+        type="text"
+        value={title}
+        className="input"
+        onChange={getInputValueHandler}
+      />
+      <FormInput
+        labelText="Price"
+        name="price"
+        type="text"
+        value={price}
+        className="input"
+        onChange={getInputValueHandler}
+      />
+      <FormInput
+        labelText="Image"
+        name="image"
+        type="file"
+        value={image}
+        className="file"
+        onChange={getInputValueHandler}
+      />
+      <button type="submit" className="btn">
+        Submit
+      </button>
     </form>
   );
 };
--- client/src/components/form/FormInput.js
+++ client/src/components/form/FormInput.js
@@ -1,10 +1,16 @@
 import React from "react";
 
-const FormInput = ({labelText, name, type, className}) => {
+const FormInput = ({ labelText, name, type, className, value, onChange }) => {
   return (
     <div className="input-container">
-      <label htmlFor={name} >{labelText}</label>
-      <input type={type} name={name} className={className}/>
+      <label htmlFor={name}>{labelText}</label>
+      <input
+        type={type}
+        name={name}
+        className={className}
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 };
